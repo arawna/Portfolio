2580d6fb526286a8798a5f8e43ce5c7234355a62
Fatih Kayan <fatih.kayan83@gmail.com>
2021-06-07 14:16:50 +0300

    emailJs entegrasyonu

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   UilPhone,
   UilEnvelope,
   UilMapMarker,
   UilMessage,
 } from "@iconscout/react-unicons";
+import emailjs from "emailjs-com";
 
 export default function ContactMe() {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    project: "",
+    message: "",
+  });
+
+  const handleChange = (e, name) => {
+    setState({
+      ...state,
+      [name]: e,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    emailjs
+      .sendForm(
+        "service_axh3m8o",
+        "template_kpqsuvz",
+        e.target,
+        "user_x9c6VLB55vR0H0SYjWACs"
+      )
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <section className="section contact" enum-data={6}>
       <h2 className="section__title">Contact Me</h2>
@@ -42,36 +76,57 @@ export default function ContactMe() {
           </div>
         </div>
 
-        <form className="contact__form grid">
+        <form className="contact__form grid" onSubmit={handleSubmit}>
           <div className="contact__inputs grid">
             <div className="contact__content">
               <label htmlFor="" className="contact__label">
                 Name
               </label>
-              <input type="text" className="contact__input" />
+              <input
+                type="text"
+                name="name"
+                className="contact__input"
+                onChange={(e) => handleChange(e.target.value, "name")}
+              />
             </div>
             <div className="contact__content">
               <label htmlFor="" className="contact__label">
                 Email
               </label>
-              <input type="email" className="contact__input" />
+              <input
+                type="email"
+                name="email"
+                className="contact__input"
+                onChange={(e) => handleChange(e.target.value, "email")}
+              />
             </div>
           </div>
           <div className="contact__content">
             <label htmlFor="" className="contact__label">
               Project
             </label>
-            <input type="text" className="contact__input" />
+            <input
+              type="text"
+              name="project"
+              className="contact__input"
+              onChange={(e) => handleChange(e.target.value, "project")}
+            />
           </div>
           <div className="contact__content">
             <label htmlFor="" className="contact__label">
               Message
             </label>
-            <textarea cols="0" rows="7" className="contact__input" />
+            <textarea
+              cols="0"
+              rows="7"
+              name="message"
+              className="contact__input"
+              onChange={(e) => handleChange(e.target.value, "message")}
+            />
           </div>
 
           <div>
-            <button className="button button__flex">
+            <button className="button button__flex" type="submit">
               Send Message
               <UilMessage className="button__icon" />
             </button>
